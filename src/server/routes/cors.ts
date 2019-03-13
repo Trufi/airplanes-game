2c5be08a36ed18974898b81be5b3cb5ad466cb4b
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-03-13 17:42:25 +0700

    Allow all origins for cors

--- src/server/routes/cors.ts
+++ src/server/routes/cors.ts
@@ -1,12 +1,6 @@
 import { Request, Response } from 'express';
 
 const allowedOrigins = [
-  'http://10.54.6.172:3000',
-  'http://10.54.6.172:3001',
-  'http://10.54.6.172:3002',
-  'http://localhost:3001',
-  'http://localhost:3000',
-  'http://localhost:3002',
   'http://demo.webmaps-dev.os-n3.hw:3002',
   'http://demo.webmaps-dev.os-n3.hw:3001',
   'http://demo.webmaps-dev.os-n3.hw:3000',
@@ -15,15 +9,24 @@ const allowedOrigins = [
 export const setAccessAllowOrigin = (req: Request, res: Response) => {
   // В заголовок Access-Control-Allow-Origin можно передовать только строку и только 1 домен.
   // Делаем динамический 'Access-Control-Allow-Origin' как аналог в .htaccess
-  // У нас есть ряд одобренных Origins, поэтому если текущий Origin есть в списке, то мы устанавливаем его.
+  // У нас есть ряд одобренных Origins, поэтому если текущий Origin есть в списке,
+  // то мы устанавливаем его.
   // Достаем текущий origin из заголовока Origin текущего запроса, с которого нам шлют запрос.
-  const allowOrigin = allowedOrigins.find((origin) => origin === req.get('Origin'));
-  // Заголовок ответа Access-Control-Allow-Headers используется в ответ на preflight request (обычно это OPTIONS),
-  // чтобы указать, какие заголовки HTTP могут использоваться во время фактического запроса.
+  const reqOrigin = req.get('Origin');
+
+  const allowOrigin = reqOrigin;
+
+  // @TODO: раскомментить строку ниже для production мода
+  // const allowOrigin = allowedOrigins.find((origin) => origin === reqOrigin);
+
+  // Заголовок ответа Access-Control-Allow-Headers используется в ответ на preflight reques
+  // (обычно это OPTIONS), чтобы указать, какие заголовки HTTP могут использоваться во время
+  // фактического запроса.
   if (req.method === 'OPTIONS') {
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      'Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With,' +
+        'Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     );
   }
 
