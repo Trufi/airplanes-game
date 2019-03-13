0d81533eb447137cb993b0a092a18d87af1e4672
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-03-14 00:34:41 +0700

    Decrease tiles search number

--- src/client/game/view.ts
+++ src/client/game/view.ts
@@ -137,8 +137,6 @@ export const createMap = () => {
 
   const container = document.getElementById('map') as HTMLElement;
   const options: Partial<MapOptions> = {
-    tileSearchNumber: 3,
-    universeTileSearchNumber: 1,
     center: [82.920412, 55.030111],
     zoom: 17,
     sendAnalytics: false,
