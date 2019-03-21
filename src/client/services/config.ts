c0b3faa60f4be19c0d0791abe8a19383d5e40675
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-03-21 16:13:33 +0700

    Fix api port

--- src/client/services/config.ts
+++ src/client/services/config.ts
@@ -1,3 +1,8 @@
 export const services = {
-  apiDomain: `${location.protocol}//${location.hostname}:3002`,
+  apiDomain: `${location.protocol}//${location.host}`,
 };
+
+// Если дев сборка, то порт будет 3000, а сервак смотрит на 3002
+if (location.port === '3000') {
+  services.apiDomain = services.apiDomain.replace('3000', '3002');
+}
