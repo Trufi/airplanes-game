e1eee840cd069e447b995d598b4d1712958898a4
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-03-28 13:27:10 +0700

    Fix pbf build

--- src/protobuf/build.js
+++ src/protobuf/build.js
@@ -9,7 +9,9 @@ const inputPath = path.join(__dirname, './source');
 const outputPath = path.join(__dirname);
 
 fs.readdirSync(inputPath).forEach((name) => {
-  const proto = schema.parse(fs.readFileSync(path.join(inputPath, name)));
-  const code = compile.raw(proto);
-  fs.writeFileSync(path.join(outputPath, name + '.js'), code, 'utf8');
+  if (path.extname(name) === '.proto') {
+    const proto = schema.parse(fs.readFileSync(path.join(inputPath, name)));
+    const code = compile.raw(proto);
+    fs.writeFileSync(path.join(outputPath, name + '.js'), code, 'utf8');
+  }
 });
