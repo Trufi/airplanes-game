445fffc92fac3d86070791dbdcf1d7453e0d6709
Михайлов Андрей Андреевич <a.mikhaylov@2gis.ru>
2019-03-29 16:31:30 +0700

    Added Protector from DDoS.

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,16 @@ export const mainServer = {
    * Если игровой сервер не прислал информацию о себе за это время, то удаляем его
    */
   clearGameThreshold: 30000,
+
+  protector: {
+    windowMs: 10 * 60 * 1000, // 10 minutes,
+    edgeCountRequest: 600, // limit each IP to 600 requests per windowMs
+    delayMs: 500, // begin adding 500ms of delay per request above 100:
+    // request # 101 is delayed by  500ms
+    // request # 102 is delayed by 1000ms
+    // request # 103 is delayed by 1500ms
+    // etc.
+  },
 };
 
 export const gameServer = {
--- src/mainServer/middlewares/index.ts
+++ src/mainServer/middlewares/index.ts
@@ -3,8 +3,32 @@ import * as expressSession from 'express-session';
 import * as passport from 'passport';
 import { json, urlencoded } from 'body-parser';
 import * as compression from 'compression';
+import * as RateLimit from 'express-rate-limit';
+import * as slowDown from 'express-slow-down';
+import { mainServer } from '../../config';
+
+const rateLimiter = new RateLimit({
+  windowMs: mainServer.protector.windowMs,
+  max: mainServer.protector.edgeCountRequest,
+  keyGenerator: (req) => {
+    return req.ip;
+  },
+});
+
+const speedLimiter = slowDown({
+  windowMs: mainServer.protector.windowMs,
+  delayAfter: mainServer.protector.edgeCountRequest,
+  delayMs: mainServer.protector.delayMs,
+  keyGenerator: (req) => {
+    return req.ip;
+  },
+});
 
 export function applyMiddlewares(app: Express) {
+  app.enable('trust proxy');
+  app.use(speedLimiter);
+  app.use(['/api/register', '/api/login'], rateLimiter);
+  app.use(['/api/register', '/api/login'], speedLimiter);
   app.use(compression());
   app.use(expressSession({ secret: 'SOME_SECRET', resave: true, saveUninitialized: true }));
   app.use(urlencoded({ extended: false }));
